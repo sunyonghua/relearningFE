65f40600818f2798bff3072221b0d584ffaf2423
sunyonghua <yonghuasir@icloud.com>
2019-04-08 10:16:16 +0800

    undefined补充

--- 数据类型/undefined.js
+++ 数据类型/undefined.js
@@ -1,3 +1,4 @@
+// undefined表示变量声明但未初始化时的值，
 // console.log(a) // a is not defined
 var a
 console.log(a) //undefined 
@@ -14,3 +15,5 @@ console.log(typeof String(undefined)) // string
 console.log(String(undefined).valueOf()) // string 
 console.log(String(undefined).toString()) // string 
 console.log(Boolean(undefined)) // false 
+console.log(undefined == 0) // undefined 不等于 0 
+// 实际上，undefined值是派生自null值的，ECMAScript标准规定对二者进行相等性测试要返回true，即 null==undefined true
