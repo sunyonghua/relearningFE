5f4cef7d3cef5226ccd91b5efec243595cf1718a
sunyonghua <yonghuasir@icloud.com>
2019-06-05 20:59:49 +0800

    array

---
+++ 数据类型/Array.js
@@ -0,0 +1,15 @@
+let arr = [1, 2, 3, 4];
+// console.log(arr);
+// console.log(typeof arr);
+// console.log('instanceof Object:', arr instanceof Object);
+// console.log('instanceof Array:', arr instanceof Array);
+// console.log('isArray:', Array.isArray(arr));
+
+let arr1 = { a: 1, b: 2 }
+console.log('isArray:', Array.isArray(arr1))
+
+
+var colors = ['red', 'blue', 'yellow'];
+console.log(colors.toString());
+console.log(colors.valueOf());
+console.log(colors.toLocaleString());
